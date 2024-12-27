cb8748f07f9a48e0b28810a2391b3085d3773b1c
javohir1606 <javohirallayorov1606@gmail.com>
2024-12-27 21:09:06 +0500

    main

---
+++ src/app/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { Bar, BarChart, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Chart } from "@/components/Chart/chart";
+import { BarChartMIne } from "@/components/Bar-chart/bar-chart";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "hsl(var(--chart-2))",
+  },
+} satisfies ChartConfig;
+
+export default function Home() {
+  return (
+    <>
+      <div className="container">
+        <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+            <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          </BarChart>
+        </ChartContainer>
+        <div className="my-[50px]">
+          <Chart />
+        </div>
+        <div className="my-[50px]">
+          <BarChartMIne />
+        </div>
+      </div>
+    </>
+  );
+}
